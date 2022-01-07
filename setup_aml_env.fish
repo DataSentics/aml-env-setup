6940776913521c385383ee729c00c68902e410f2
Jakub Hejhal <kubic71@gmail.com>
2022-01-07 04:02:56 +0100

    bugfix

--- setup_aml_env.fish
+++ setup_aml_env.fish
@@ -1,13 +1,13 @@
 #!/usr/bin/env fish
 
 echo "Copying setup scripts"
-rsync --delete -r ~/psi/aml_env_setup azure-ml:~/
+rsync --delete -r ~/psi/aml-env-setup azure-ml:~/
 
 
 # echo "Copying personal-shelf-inspector-training files"
 # azsync
 
 echo "Running the setup scripts"
-ssh azure-ml "cd ~/aml_env_setup && bash setup.sh"
+ssh azure-ml "cd ~/aml-env-setup && bash setup.sh"
 
 
